aed91bd76ea63554b8bf7564adb0d653e8f6c9ae
maestroprog <maestroprog@naver.com>
2019-07-21 16:54:00 +0900

    install material-UI and bootstrap

--- volf_fullstack/backend/routes/notices.js
+++ volf_fullstack/backend/routes/notices.js
@@ -9,7 +9,7 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
 	const title = req.body.title;
-	const article = req.body.title;
+	const article = req.body.article;
 	const author = req.body.author;
 	const newNotice = new Notice({
 		title,
@@ -18,7 +18,33 @@ router.route('/add').post((req, res) => {
 	});
 
 	newNotice.save()
-	.then(() => res.json('Exercise added!'))
+	.then(() => res.json('Notice added!'))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+	Notice.findById(req.params.id)
+		.then(notice => res.json(notice))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+	Notice.findByIdAndDelete(req.params.id)
+		.then(() => res.json('Notice deleted.'))
+		.catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+	Notice.findById(req.params.id)
+		.then(notice => {
+			notice.title = req.body.title;
+			notice.article = req.body.article;
+			notice.author = req.body.author;
+			
+			notice.save()
+				.then(() => res.json('Notice modified!'))
+				.catch(err => res.status(400).json('Error: '+ err));
+		})
 		.catch(err => res.status(400).json('Error: ' + err));
 });
 
